0769f66eab1c75c4884153e47f2b3bb169399e00
maorSeri <maorroam14@gmail.com>
2018-05-17 09:00:45 +0300

    initial commit

---
+++ Models/Student.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FinedProjectApp.Models
+{
+    public class Student
+    {
+        /*
+         * students personal details
+         */
+         //students profile nekname.
+        [Key]
+        public string UserName { get; set; }
+        
+        //student's password
+        [Required]
+        public string Password { get; set; }
+        //students id
+        [Required]
+        public int Id { get; set; }
+        //students email
+        [Required]
+        public string Email { get; set; }
+        //students first name
+        [Required]
+        public string FirstName { get; set; }
+        //students last name
+        [Required]
+        public string LastName { get; set; }
+        //students gender: male/female
+        [Required]
+        public string Gender { get; set; }
+        //or to put the birthday
+        public int BirthDate { get; set; }
+        /*
+         * students professional information
+         */
+        
+        //student degree average
+        [Required]
+        public int Average { get; set; }
+        //students grup size
+        [Required]
+        public int GroupSize { get; set; }
+        //student elective courses
+        [Required]
+        public string Courses { get; set; }
+        //Locations where student is available for meating.
+        [Required]
+        public string Location { get; set; }
+        //student preference in fild of project: industry/ reserch 
+        [Required]
+        public string FieldOfProject { get; set; }
+        //days where student is free for working on project.
+        [Required]
+        public string FreeDays { get; set; }
+        
+        //amount of years of experience
+        [Required]
+        public string Experience { get; set; }
+        //programming languages that student is familiar whith
+        [Required]
+        public string ProgrammingLanguage { get; set; }
+        
+    }
+}
---
+++ Scripts/Register.js
@@ -0,0 +1,49 @@
+﻿
+
+ko.validation.rules['mustEqual'] = {
+    validator: function (val, otherVal) {
+        return val === otherVal;
+    },
+    message: "Passwords don't match!"
+};
+
+ko.validation.registerExtenders();
+
+function AppViewModel() {
+    var that = this;
+    this.UserName = ko.observable("").extend({ required: { message: "Please enter a UserName" } });
+    this.Password = ko.observable("").extend({ required: { message: "Please enter your Password" } });
+    this.Password2 = ko.observable("").extend({
+        required: { message: "Passwords don't match!" },
+        mustEqual: that.Password
+
+    });
+    this.Email = ko.observable("").extend({ required: { message: "Please enter your Password" } });
+    this.Options = ko.observableArray(["student", "project manager"]);
+    this.selected = ko.observable("");
+    this.RegBtn = function () {
+        var JsUser = ko.toJS(this.UserName);
+        var JsPass = ko.toJS(this.Password);
+        var JsEmail = ko.toJS(this.Email);
+        var JsonData = {
+            "UserName": JsUser,
+            "Password": JsPass,
+            "Email": JsEmail
+        };
+        sessionStorage.UserName = ko.toJS(that.UserName);
+        sessionStorage.Password = ko.toJS(that.Password);
+        sessionStorage.Email = ko.toJS(that.Email);
+        if (this.selected == "student") {
+            location.replace("../View/Student.html");
+        } else {
+            location.replace("../View/ProjectDirector.html");
+        }
+    };
+
+    this.isFormValid = ko.computed(function () {
+
+        return this.Password() && this.Password2() && this.UserName() && this.Email() && this.Password2.isValid();
+    }, this);
+}
+
+ko.applyBindings(new AppViewModel());
